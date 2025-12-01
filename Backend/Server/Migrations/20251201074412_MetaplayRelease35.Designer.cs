fa95001dd2c3a07fc9c517f95bf10f0bfe929e01
Jurjen Biewenga <jurjen.biewenga@metaplay.io>
2025-12-01 10:39:55 +0200

    r35 migration

--- Backend/Server/Migrations/20251201074412_MetaplayRelease35.Designer.cs
+++ Backend/Server/Migrations/20251201074412_MetaplayRelease35.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Game.Server.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Server.Migrations
 {
     [DbContext(typeof(GameDbContext))]
-    partial class GameDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251201074412_MetaplayRelease35")]
+    partial class MetaplayRelease35
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.9");
---
+++ Backend/Server/Migrations/20251201074412_MetaplayRelease35.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class MetaplayRelease35 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<sbyte>(
+                name: "DeletionSource",
+                table: "Players",
+                type: "tinyint",
+                nullable: true);
+
+            migrationBuilder.AddColumn<sbyte>(
+                name: "LifecycleStatus",
+                table: "Players",
+                type: "tinyint",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ScheduledForDeletionAt",
+                table: "Players",
+                type: "DateTime",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeletionSource",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "LifecycleStatus",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "ScheduledForDeletionAt",
+                table: "Players");
+        }
+    }
+}
